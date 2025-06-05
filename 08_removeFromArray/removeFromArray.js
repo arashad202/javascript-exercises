e182930b1e6c6b0db11a3322e1b88bca78296ccf
arashad202 <ahm.rashad.202@gmail.com>
2025-06-06 00:45:45 +0300

    RemoveFromArray
    Filter array from items in the rest of args.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    return arr.filter((arrItem) =>  !items.includes(arrItem));
 };
 
 // Do not edit below this line
