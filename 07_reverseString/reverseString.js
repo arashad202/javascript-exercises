10e0919df4fb38fc41c9e17f3f3b09218fe9d049
arashad202 <ahm.rashad.202@gmail.com>
2025-06-06 00:34:54 +0300

    ReverseString
    Split string then reverse then join again.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    let sentenceArray = sentence.split('');
+    let reversedArray = sentenceArray.reverse();
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
